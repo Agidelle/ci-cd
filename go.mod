8f4d49c7458169011fa38d58768c4495664c7d78
Agidelle <agidelleds@gmail.com>
2025-03-29 11:04:19 +0300

    change

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agidelle/ci-cd
+
+go 1.24.0
--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package ci_cd
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(hello())
+}
+
+func hello() string {
+	return "Hello go"
+}
